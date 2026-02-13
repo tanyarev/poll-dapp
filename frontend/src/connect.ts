8c176142713cb9a2f9f961dcd682a3b05acd32b1
tanyarev <tatjana_revenkova@rambler.ru>
2026-02-14 01:19:07 +0300

    Create connect.ts

---
+++ frontend/src/connect.ts
@@ -0,0 +1,9 @@
+import { showConnect } from '@stacks/connect';
+
+export const connectWallet = () => {
+  showConnect({
+    appDetails: { name: 'Stacks Poll DApp', icon: window.location.origin + '/logo.png' },
+    redirectTo: '/',
+    onFinish: () => window.location.reload(),
+  });
+};
